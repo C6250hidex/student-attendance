c927610152175b469aef3687334deca11cfb2aea
Nnamani Emmanuel <chidex6250@gmail.com>
2026-07-05 08:19:06 +0100

    made changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // This line is critical for deployment!
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://student-attendance-api-g8bh.onrender.com/api",
   withCredentials: true,
 });
 
